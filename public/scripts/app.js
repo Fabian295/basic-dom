301bf5f98b0ea1981db5bf336790910955b61ca4
Fabian295 <fabiankanhai70@gmail.com>
2022-11-09 16:30:40 +0100

    Add/Configure Babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,9 @@
-console.log('app-js for react!')
-{/* <p>This is JSX from app.js</p> */}
-const template = React.createElement('p', null, 'This is JSX from app.js via CDN')
+"use strict";
 
-const DOMContainer = document.getElementById('app')
-const root = ReactDOM.createRoot(DOMContainer)
-root.render(template)
+console.log('app-js for react!');
+var template = /*#__PURE__*/React.createElement("p", null, "This is JSX compiled to public folder app.js with config file from Babel");
+// const template = React.createElement('p', null, 'This is JSX from app.js via CDN')
+
+var DOMContainer = document.getElementById('app');
+var root = ReactDOM.createRoot(DOMContainer);
+root.render(template);
