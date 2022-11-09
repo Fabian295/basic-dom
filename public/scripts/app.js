315a13cc32207907a239f51be439926e425309b1
Fabian295 <fabiankanhai70@gmail.com>
2022-11-09 15:15:48 +0100

    update text from template component

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,6 @@
 console.log('app-js for react!')
 {/* <p>This is JSX from app.js</p> */}
-const template = React.createElement('p', null, 'This is JSX from app.js')
+const template = React.createElement('p', null, 'This is JSX from app.js via CDN')
 
 const DOMContainer = document.getElementById('app')
 const root = ReactDOM.createRoot(DOMContainer)
