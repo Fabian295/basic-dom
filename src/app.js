c35d1a6991ae22cb557909887cb3de784566bb33
Fabian295 <fabiankanhai70@gmail.com>
2022-11-09 17:15:27 +0100

    Class Component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 console.log('app-js for react!')
-const template = <p>This is JSX compiled to public folder app.js with config file from Babel</p> 
+// const template = <p>This is JSX compiled to public folder app.js with config file from Babel</p> 
 // const template = React.createElement('p', null, 'This is JSX from app.js via CDN')
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="hello">
+          Hello World!
+        </h1>
+      </div>
+    )
+  }
+}
 
 const DOMContainer = document.getElementById('app')
 const root = ReactDOM.createRoot(DOMContainer)
-root.render(template)
+root.render(<Hello />)
