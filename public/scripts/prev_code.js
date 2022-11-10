0b345c7e7ad98770d5c84fc7eae6b271a16802c7
Fabian295 <fabiankanhai70@gmail.com>
2022-11-10 16:00:03 +0100

    Clear app.js, cleared code moved to prev_code.js

--- public/scripts/prev_code.js
+++ public/scripts/prev_code.js
@@ -54,3 +54,40 @@ const Movie = (props) => {
    
   )
 }
+
+class Checkbox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      checked: true,
+    }
+    this.handleCheck = this.handleCheck.bind(this)
+  }
+  handleCheck() {
+    this.setState({
+      checked: !this.state.checked
+    })
+
+  }
+  render() {
+    let msg
+    if(this.state.checked) {
+      msg = 'CHECKED'
+    } else {
+      msg = "NOT CHECKED"
+    }
+    return (
+      <div>
+        <input  type="checkbox" 
+        onChange={this.handleCheck}
+        defaultChecked={this.state.checked} />
+        <p>The box is {msg}</p>
+      </div>
+    )
+  }
+}
+
+
+const DOMContainer = document.getElementById('app')
+const root = ReactDOM.createRoot(DOMContainer)
+root.render(<Checkbox  />)
