d5d48881c2d4fed093e5061f2b8581fc1ef3f92a
Fabian295 <fabiankanhai70@gmail.com>
2022-11-11 07:55:26 +0100

    simple template with basic state functionality, events and form input

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,43 @@
 "use strict";
 
 console.log('app-js running!');
+var app = {
+  title: 'BLA',
+  subTitle: 'Let program decide your activities',
+  options: []
+};
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  console.log('Form Submitted!!');
+  var option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    console.log(app.options.length);
+    e.target.elements.option.value = '';
+    render();
+  }
+};
+var handleRemove = function handleRemove(e) {
+  // app.options.splice(0, app.options.length)
+  // console.log(e.target)
+  app.options = [];
+  console.log(app.options);
+  console.log(app.options.length);
+  render();
+};
 var DOMContainer = document.getElementById('app');
 var root = ReactDOM.createRoot(DOMContainer);
-root.render( /*#__PURE__*/React.createElement(Checkbox, null));
+var render = function render() {
+  var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "The BLA"), app.subTitle && /*#__PURE__*/React.createElement("p", null, app.subTitle), /*#__PURE__*/React.createElement("p", null, app.options.length > 0 ? 'Here are your options : ' : 'There are no options!'), /*#__PURE__*/React.createElement("p", null, app.options.length), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, app.options[0]), /*#__PURE__*/React.createElement("li", null, app.options[1])), /*#__PURE__*/React.createElement("form", {
+    action: "",
+    onSubmit: onFormSubmit
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "option",
+    id: ""
+  }), /*#__PURE__*/React.createElement("button", null, "Add Option")), /*#__PURE__*/React.createElement("button", {
+    onClick: handleRemove
+  }, "Remove Option"));
+  root.render(template);
+};
+render();
--- src/app.js
+++ src/app.js
@@ -1,7 +1,63 @@
 console.log('app-js running!')
 
+const app = {
+  title: 'BLA',
+  subTitle: 'Let program decide your activities',
+  options: []
+}
 
+const onFormSubmit = (e) => {
+  e.preventDefault()
+  console.log('Form Submitted!!')
+
+  const option = e.target.elements.option.value;
+
+  if(option) {
+    app.options.push(option)
+    console.log(app.options.length)
+
+    e.target.elements.option.value = '';
+    render();
+  }
+}
+
+const handleRemove = (e) => {
+  // app.options.splice(0, app.options.length)
+  // console.log(e.target)
+  app.options = []
+  console.log(app.options)
+  console.log(app.options.length)
+  render()
+}
 
 const DOMContainer = document.getElementById('app')
 const root = ReactDOM.createRoot(DOMContainer)
-root.render(<Checkbox  />)
+
+const render = () => {
+  const template = (
+    <div>
+      <h1>The BLA</h1>
+      { app.subTitle && <p>{ app.subTitle }</p> }
+      <p>{ app.options.length > 0 ? 'Here are your options : ' : 'There are no options!' }</p>
+      <p>{ app.options.length }</p>
+  
+      <ol>
+        <li>{ app.options[0] }</li>
+        <li>{ app.options[1] }</li>
+      </ol>
+  
+      <form action="" onSubmit={onFormSubmit}>
+        <input type="text" name="option" id="" />
+        <button >Add Option</button>
+      </form>
+        <button  onClick={handleRemove}>Remove Option</button>
+    </div>
+  );
+
+  root.render( template )
+};
+
+render();
+
+
+
